04c8d2be1765b89ebdb920e0f42309c9443700c4
hjeglinton <124302914+hjeglinton@users.noreply.github.com>
2024-01-12 09:10:09 -0500

    Add documentation

--- R/fasterrisk.R
+++ R/fasterrisk.R
@@ -7,41 +7,63 @@
 #use_virtualenv("FasterRisk-environment")
 
 #py_install("fasterrisk", pip=TRUE, envname="FasterRisk-environment")
-fasterrisk <- import("fasterrisk")
-np <- import("numpy", convert=FALSE)
+#fasterrisk <- import("fasterrisk")
+#np <- import("numpy", convert=FALSE)
 
-run_FR <- function(X_train, y_train, X_test, lb, ub) {
+
+#' Run FasterRisk
+#' 
+#' Implements FasterRisk method to learn risk score model. 
+#' @param X_train Numeric matrix of training data coefficients. Must contain
+#' an intercept column. 
+#' @param y_train Numeric vector of outcomes from the training data (0/1). 
+#' @param X_test Optional, numeric matrix of coefficients from the test data. If
+#' not supplied, the function will return predicted probabilities using the 
+#' training data coefficients. 
+#' @return List with three attributes: 
+#' * `coef`: Numeric vector of logistic regression coefficients
+#' * `integer_coef`: Numeric vector of integer coefficients. 
+#' * `pred_test`: Numeric vector with predicted probabilities calculated from the 
+#' `X_test` matrix, if supplied, or the `X_train` matrix. 
+run_FR <- function(X_train, y_train, X_test = NULL, lb, ub) {
   
+  if (is.null(X_test)) {
+    X_test <- X_train
+  }
+  
+  # Reformat outcome data to -1/1
   y_train <- case_when(y_train == 0 ~ -1,
                        y_train == 1 ~ 1)
   
+  # Reformat to np arrays
   X_train <- np$array(X_train)
   y_train <- np$array(y_train)
   
-  m <- fasterrisk$fasterrisk$RiskScoreOptimizer(X = X_train, y = y_train, k = dim(X)[2]-1, 
+  # Run FasterRisk method
+  m <- fasterrisk$fasterrisk$RiskScoreOptimizer(X = X_train, y = y_train, 
+                                                k = dim(X)[2]-1, 
                                                 lb = lb, ub = ub)
-  
   m$optimize()
 
   
-  # get all top m solutions from the final diverse pool
+  # Get all solutions
   solutions = m$get_models()
   multipliers = solutions[1][[1]]
   sparseDiversePool_beta0_integer = solutions[2][[1]]
   sparseDiversePool_betas_integer = solutions[3][[1]]
   
-  model_index = 1 # first model
+  # Get top soluation
+  model_index = 1
   multiplier = multipliers[model_index]
   intercept = sparseDiversePool_beta0_integer[model_index]
   coefficients = np$array(sparseDiversePool_betas_integer[model_index, ])
-  
   coefficients[0] = intercept
   
-
+  # Calculate predicted probabilities of test set
   RiskScoreClassifier_m = fasterrisk$fasterrisk$RiskScoreClassifier(multiplier, intercept, coefficients)
   predicted_probs_test <- RiskScoreClassifier_m$predict_prob(X_test)
   
-
+  # Return list with logistic model, integer model, and predicted probabilities
   return(list(coef = as.numeric(coefficients/multiplier),
               integer_coef = as.numeric(coefficients),
               pred_test = predicted_probs_test))
