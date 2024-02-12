3635a981bde99b7a517a54d481a70a7dfc307541
hjeglinton <124302914+hjeglinton@users.noreply.github.com>
2024-02-12 15:51:19 -0500

    Add preprocessing for new data

--- R/tb_preprocessing.R
+++ R/tb_preprocessing.R
@@ -94,3 +94,98 @@ tb_as_matrix <- function(tb_df) {
   
 }
 
+mode_imputation <- function(x) {
+  ux <- unique(x)
+  mode <- ux[which.max(tabulate(match(x, ux)))]
+  
+  new_x <- replace_na(x, mode)
+  return(new_x)
+}
+
+kharitode_preprocessing <- function(tb_df) {
+  # Select variables and rename
+  tb_df <- tb_df %>% select(c(xpert_status_fac, age_group, sex, hiv_status_fac,
+                              other_conditions_fac___3, symp_fac___1, symp_fac___2,
+                              symp_fac___3, symp_fac___4, length_symp_wk_fac, 
+                              length_symp_days_fac, length_symp_unit_fac, smk_fac, 
+                              dx_tb_past_fac, educ_fac)) %>%
+    rename(tb = xpert_status_fac, hiv_pos = hiv_status_fac,
+           diabetes = other_conditions_fac___3, cough = symp_fac___1,
+           fever = symp_fac___2, weight_loss = symp_fac___3, 
+           night_sweats = symp_fac___4, ever_smoke = smk_fac, past_tb = dx_tb_past_fac,
+           education = educ_fac)
+  
+  tb_df$tb <- case_when(tb_df$tb == 1 ~ 1, tb_df$tb == 2 ~ 0)
+  tb_df$male <- case_when(tb_df$sex == 1 ~ 1, tb_df$sex == 2 ~ 0)
+  tb_df$hiv_pos <- case_when(tb_df$hiv_pos == 1 ~ 1, tb_df$hiv_pos %in% c(2,77) ~ 0)
+  tb_df$ever_smoke <- case_when(tb_df$ever_smoke %in% c(1,2) ~ 1, 
+                                tb_df$ever_smoke == 3 ~ 0)
+  tb_df$past_tb <- case_when(tb_df$past_tb == 1 ~ 1, tb_df$past_tb == 2 ~ 0)
+  
+  # Categorize education to HS and above
+  tb_df$hs_less <- case_when(tb_df$education <= 12 ~ 1,
+                             tb_df$education %in% c(13, 14) ~ 0,
+                             TRUE ~ NA)
+  tb_df <- tb_df %>% select(-c(education))
+  
+  # Categorize number of weeks
+  tb_df$two_weeks_symp <- case_when(tb_df$length_symp_days_fac <= 14 | 
+                                      tb_df$length_symp_wk_fac < 2 ~ 0, 
+                                    tb_df$length_symp_unit_fac == 77 |
+                                      tb_df$length_symp_wk_fac == 999 ~ NA,
+                                    TRUE ~ 1)
+  tb_df <- tb_df %>% select(-c(length_symp_wk_fac, length_symp_days_fac,
+                               length_symp_unit_fac, sex))
+  
+  # Total number of symptoms
+  tb_df$num_symptoms <- tb_df$fever + tb_df$weight_loss +
+    tb_df$cough+tb_df$night_sweats
+  tb_df <- tb_df %>% select(-c(night_sweats, weight_loss, cough, fever))
+  
+  # All factors
+  tb_df[] <- lapply(tb_df, function(x){return(as.factor(x))})
+  tb_df$age_group <- relevel(tb_df$age_group, ref="[55,99)")
+  
+  # Single imputation (mode)
+  tb_df <- tb_df %>%
+    mutate_all(mode_imputation)
+  
+  return(tb_df)
+  
+}
+
+stomp_preprocessing <- function(df) {
+  
+  # Select variables and rename
+  df <- df %>%
+    select(why_a_case_xpert, com_why_a_case_xpert,
+           why_a_ctrl_xpert, com_why_a_ctrl_xpert,
+           age_group, sex_female, hivcat1, medical_conditions___3,
+           cough = symptoms_past_week___1, fever = symptoms_past_week___3,
+           night_sweats = symptoms_past_week___5, weight_loss, 
+           smoked_tobacco_in_past, treated_past, highest_grade_education,
+           cough_weeks, weightloss_weeks, nightsweats_weeks, feverchills_weeks) %>%
+    
+    rename(hiv_pos = hivcat1, diabetes = medical_conditions___3, 
+           ever_smoke = smoked_tobacco_in_past, past_tb = treated_past) %>%
+    
+    mutate(tb = case_when(why_a_case_xpert == 1 | com_why_a_case_xpert == 1 ~ 1,
+                          why_a_ctrl_xpert == 2 | com_why_a_ctrl_xpert == 2 ~ 0),
+           male = case_when(sex_female == 1 ~ 0,
+                            sex_female == 0 ~ 1),
+           hs_less = case_when(highest_grade_education < 5 ~ 1,
+                               highest_grade_education >= 5 ~ 0),
+           num_symptoms = cough + fever + night_sweats + weight_loss,
+           two_weeks_symp = case_when(cough_weeks >= 2 | weightloss_weeks >= 2 |
+                             nightsweats_weeks >= 2 | feverchills_weeks >= 2 ~ 1,
+                             TRUE ~ 0)) %>%
+    
+    select(tb, age_group, hiv_pos, diabetes, ever_smoke, past_tb, male, hs_less,
+           num_symptoms, two_weeks_symp)
+  
+  return(df)
+  
+  
+}
+
+
